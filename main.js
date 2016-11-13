6519c1bd9a62d2bd0d919b46d6eb94cca7db85ff
Tomf <tomf@curiousgaming.co.uk>
2016-11-13 12:38:14 +0000

    sprite on the screen

--- main.js
+++ main.js
@@ -3,17 +3,39 @@ var stage;
 
 function init() {
 	stage = new createjs.Stage("stagecanvas");
+	
+	manifest = [
+		{src: "red_squire.png", id: "grant"},
+	];
+	
+	//to do: load multiple assets
+	var player = new Image();
+    player.src = "assets/red_squire.png";
+    player.onload = handleComplete;
+	
+	drawDude();
+}
+
+function handleComplete(event) {
+	var image = event.target;
+	var player = new createjs.Bitmap(image);
+	stage.addChild(player);
+	player.x = 200;
+	player.y = 100;
+	
+	drawCircle();
+}
+
+function drawDude() {
+	
+}
+
+function drawCircle() {
+	//debug function
 	var circle = new createjs.Shape();
 	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
 	circle.x = 100;
 	circle.y = 100;
 	stage.addChild(circle);
 	stage.update();
-	
-	drawGrid();
 }
-
-function drawGrid() {
-
-}
-
